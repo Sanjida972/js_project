28883da5aa39853fdf19d026309058ea126c9450
Sanjida972 <s28255235@gmail.com>
2025-11-01 10:20:51 +0600

    Create product.model.js

---
+++ models/product.model.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  stock: { type: Number, required: true },
+  category: { type: String },
+});
+
+productSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: (doc, ret) => {
+    ret.id = ret._id;
+    delete ret._id;
+  },
+});
+
+const Product = mongoose.model("Product", productSchema);
+export default Product;
