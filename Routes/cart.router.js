39013fbe0ed62a9c203e3f638ed87064ad29fa38
Sanjida972 <s28255235@gmail.com>
2025-11-01 10:19:48 +0600

    Create cart.router.js

---
+++ Routes/cart.router.js
@@ -0,0 +1,21 @@
+import express from "express";
+import {
+  getCarts,
+  getCart,
+  createCart,
+  updateCart,
+  deleteCart,
+  clearCart,
+} from "../controllers/cart.controller.js";
+
+const router = express.Router();
+
+router.get("/carts", getCarts);         
+router.get("/carts/:id", getCart);        
+router.post("/carts", createCart);        
+router.put("/carts/:id", updateCart);     
+router.delete("/carts/:id", deleteCart);  
+
+router.delete("/carts/clear/:userId", clearCart); 
+
+export default router;
