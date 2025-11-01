01f88372db97bc3ddbd1f03ba727b3c50b3b28e8
Sanjida972 <s28255235@gmail.com>
2025-11-01 10:21:14 +0600

    Create order.model.js

---
+++ models/order.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+    items: [
+      {
+        productId: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
+        quantity: { type: Number, required: true },
+        price: { type: Number, required: true },
+      },
+    ],
+    totalAmount: { type: Number, required: true },
+    paymentMethod: { type: String, required: true },
+    status: { type: String, default: "Pending" },
+  },
+  { timestamps: true }
+);
+
+orderSchema.set("toJSON", {
+  virtuals: true,
+  versionKey: false,
+  transform: (doc, ret) => {
+    ret.id = ret._id;
+    delete ret._id;
+  },
+});
+
+export default mongoose.model("Order", orderSchema);
